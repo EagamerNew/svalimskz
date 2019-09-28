7b79fbab9620314f64fa19741dfe3c8a83c51674
Azamat <fakhrudinov.azamat@gmail.com>
2019-09-28 18:20:12 +0600

    last

--- front/src/app/layout/dashboard/dashboard.component.ts
+++ front/src/app/layout/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import {FormControl, Validators} from '@angular/forms';
 import {Cafedra, CAFEDRAS} from '../shared/model/cafedra';
 import {MatSelect} from '@angular/material';
 import {take, takeUntil} from 'rxjs/operators';
+import {HOBBIES, Hobby} from '../shared/model/hobby';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,16 +13,23 @@ import {take, takeUntil} from 'rxjs/operators';
 })
 export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  state = 6;
+  state = 7;
   name = new FormControl('', [Validators.required]);
+  mail = new FormControl('', [Validators.required, Validators.email]);
+  phone = new FormControl('', [Validators.required]);
   age = 16;
   univerInfo = '1';
-  private unsubscribeAll: Subject<any>;
   cafedraList: Cafedra[] = CAFEDRAS;
   public cafedraCtrl: FormControl = new FormControl();
   public cafedraFilterCtrl: FormControl = new FormControl();
   public filteredCafedras: ReplaySubject<Cafedra[]> = new ReplaySubject<Cafedra[]>(1);
-  @ViewChild('singleSelect', {static: false}) singleSelect: MatSelect;
+  @ViewChild('cafedraSelect', {static: false}) cafedraSelect: MatSelect;
+  hobbyList: Hobby[] = HOBBIES;
+  public hobbyCtrl: FormControl = new FormControl();
+  public hobbyFilterCtrl: FormControl = new FormControl();
+  public filteredHobbies: ReplaySubject<Hobby[]> = new ReplaySubject<Hobby[]>(1);
+  @ViewChild('hobbySelect', {static: false}) hobbySelect: MatSelect;
+  private unsubscribeAll: Subject<any>;
 
   constructor() {
     this.unsubscribeAll = new Subject();
@@ -32,12 +40,22 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit {
       6 state
      */
     console.log('oninit');
-    this.cafedraCtrl.setValue(this.cafedraList[2]);
+    this.cafedraCtrl.setValue(this.cafedraList[0]);
     this.filteredCafedras.next(this.cafedraList.slice());
     this.cafedraFilterCtrl.valueChanges
       .pipe(takeUntil(this.unsubscribeAll))
       .subscribe(() => {
         this.filterCafedras();
+      });
+    /*
+      7 state
+     */
+    this.hobbyCtrl.setValue(this.hobbyList[0]);
+    this.filteredHobbies.next(this.hobbyList.slice());
+    this.hobbyFilterCtrl.valueChanges
+      .pipe(takeUntil(this.unsubscribeAll))
+      .subscribe(() => {
+        this.filterHobbies();
       });
   }
 
@@ -47,8 +65,7 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    console.log('view init');
-    this.setInitialValue();
+    this.setInitialCafedraValue();
   }
 
   start() {
@@ -70,11 +87,11 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit {
   /*
     6. На кого вы учились?
    */
-  protected setInitialValue() {
+  protected setInitialCafedraValue() {
     this.filteredCafedras
       .pipe(take(1), takeUntil(this.unsubscribeAll))
       .subscribe(() => {
-        this.singleSelect.compareWith = (a: Cafedra, b: Cafedra) => a && b && a.id === b.id;
+        this.cafedraSelect.compareWith = (a: Cafedra, b: Cafedra) => a && b && a.id === b.id;
       });
   }
 
@@ -91,6 +108,33 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit {
     }
     this.filteredCafedras.next(
       this.cafedraList.filter(cafedra => cafedra.name.toLowerCase().indexOf(search) > -1)
+    );
+  }
+
+  /*
+    7. Чем вы занимаетесь сейчас?
+   */
+  protected setInitialValue() {
+    this.filteredCafedras
+      .pipe(take(1), takeUntil(this.unsubscribeAll))
+      .subscribe(() => {
+        this.hobbySelect.compareWith = (a: Hobby, b: Hobby) => a && b && a.id === b.id;
+      });
+  }
+
+  protected filterHobbies() {
+    if (!this.hobbyList) {
+      return;
+    }
+    let search = this.hobbyFilterCtrl.value;
+    if (!search) {
+      this.filteredHobbies.next(this.hobbyList.slice());
+      return;
+    } else {
+      search = search.toLowerCase();
+    }
+    this.filteredHobbies.next(
+      this.hobbyList.filter(hobby => hobby.name.toLowerCase().indexOf(search) > -1)
     );
   }
 
---
+++ front/src/app/layout/shared/model/hobby.ts
@@ -0,0 +1,15 @@
+export interface Hobby {
+  id: string;
+  name: string;
+}
+
+export const HOBBIES: Hobby[] = [
+  {id: '1', name: 'Менеджер по продажам'},
+  {id: '2', name: 'Web разработчик'},
+  {id: '3', name: 'Frontend разработчик'},
+  {id: '4', name: 'Backend разработчик'},
+  {id: '5', name: 'Full разработчик'},
+  {id: '6', name: 'Системный администратор'},
+  {id: '7', name: 'Аналитик'},
+
+];
